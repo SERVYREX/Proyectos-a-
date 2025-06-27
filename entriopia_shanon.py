99cd43ad7d2ff6cfa8aa113348f811c2c4036643
SERVYREX <codbryxp@gmail.com>
2025-06-27 00:12:36 -0400

    Update entriopia_shanon.py

--- entriopia_shanon.py
+++ entriopia_shanon.py
@@ -3,12 +3,17 @@ import numpy as np
 from pathlib import Path
 
 def entriopiacanal(m):
+    #Crea un histograma con el valor del canal ingresado
     histograma, _ = np.histogram(m, bins=256, range=(0, 256))
+    #Calcula la probabilidad normalizada 
     probabilidades = histograma / histograma.sum()
+    #Elimina las probabilidades "0"
     prob_no_cero = probabilidades[probabilidades > 0]
-    entropia = -np.sum(prob_no_cero * np.log2(prob_no_cero))
-    return entropia
+    #Calcula la entriopia
+    entriopia = -np.sum(prob_no_cero * np.log2(prob_no_cero))
+    return entriopia
 
+#Carga todas las imagenes, la imagen ejemplo y las reescaladas mediante ambas implementaciones 
 descargas = Path.home() / "Downloads"
 imgreescaladaGPU = "imagen_reescaladaGPU.png"
 imgreescaladaCPU = "imagen_reescaladaCPU.png"
@@ -21,10 +26,12 @@ ejemplo = cv2.imread(ruta1)
 reescaladaCPU = cv2.imread(ruta2)
 reescaladaGPU = cv2.imread(ruta3)
 
+#Obtiene los valores de cada canal RGB
 canal_azulej = ejemplo[:, :, 0]
 canal_verdeej = ejemplo[:, :, 1]
 canal_rojoej = ejemplo[:, :, 2]
 
+#Calcula la entriopia de cada canal, junto a su promedio
 entriopia_azulej = entriopiacanal(canal_azulej)
 entriopia_verdeej = entriopiacanal(canal_verdeej)
 entriopia_rojoej = entriopiacanal(canal_rojoej)
@@ -32,6 +39,7 @@ promedioej = (entriopia_azulej + entriopia_rojoej + entriopia_verdeej)/3
 
 print(f"Entriopia RGB-EJEMPLO: {promedioej:.3f}")
 
+#Proceso analogo para la implmentacion GPU Y CPU
 canal_azulcpu = reescaladaCPU[:, :, 0]
 canal_verdecpu = reescaladaCPU[:, :, 1]
 canal_rojocpu = reescaladaCPU[:, :, 2]
@@ -54,5 +62,6 @@ promediogpu = (entriopia_azulgpu + entriopia_rojogpu + entriopia_verdegpu)/3
 
 print(f"Entriopia RGB-GPU: {promediogpu:.3f}")
 
+#Calcula el indice de preservacion
 print(f"Preservacion CPU: {promedioej/promediocpu:.3f}")
 print(f"Preservacion GPU: {promedioej/promediogpu:.3f}")
